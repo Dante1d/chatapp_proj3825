bac10063df802fbc4d8fe593fcd0c8bdc2ca9c5b
Dante Donaldson <ddonaldsonprime@gmail.com>
2024-12-01 09:58:41 -0600

    Update client and server code with message acknowledgment and encryption improvements

--- client.py
+++ client.py
@@ -1,48 +1,46 @@
 import socket
 from threading import Thread
-# Provides us with methods for interacting with the operating system.
-import os
+from cryptography.fernet import Fernet
 
 class Client:
-  
-  # When creating a client, connect to the server, ask for
-  # a username, and begin server communication.
-  def __init__(self, HOST, PORT):
-    self.socket = socket.socket()
-    self.socket.connect((HOST, PORT))
-    self.name = input("Enter your name: ")
-    
-    self.talk_to_server()
+    def __init__(self, HOST, PORT):
+        self.socket = socket.socket()
+        self.socket.connect((HOST, PORT))
+        self.key = input("Enter the encryption key: ").encode()
+        self.cipher = Fernet(self.key)
+        self.name = input("Enter your name: ")
+        self.send_message(self.name)  # Send username to the server
+        Thread(target=self.receive_message).start()
+        self.send_message_loop()
 
-  # First send over the name of the client. Then start listening
-  # for messages on a separate thread. Message sending will be on
-  # the main thread.
-  def talk_to_server(self):
-    self.socket.send(self.name.encode())
-    Thread(target = self.receive_message).start()
-    self.send_message()
-    
-  # Get user input and send the message to the server
-  # with the client's name prepended.
-  def send_message(self):
-    while True:
-      client_input = input("")
-      client_message = self.name + ": " + client_input
-      self.socket.send(client_message.encode())
-      
-  # Constantly listen out for messages. If the message is response
-  # from the server is empty, close the program.
-  def receive_message(self):
-    while True:
-      server_message = self.socket.recv(1024).decode()
-      if not server_message.strip():
-        os._exit(0)
-      # Add some color to the console.
-      # Check if the message is a server acknowledgment
-      if server_message.startswith("Server:"):
-          print("\033[1;32;40m" + server_message + "\033[0m")  # Green for acknowledgment
-      else:
-          print("\033[1;31;40m" + server_message + "\033[0m")  # Red for regular messages
-      
+    def send_message(self, message):
+        encrypted_message = self.cipher.encrypt(message.encode())
+        self.socket.send(encrypted_message)
+
+    def send_message_loop(self):
+        while True:
+            message = input("")
+            self.send_message(message)
+            if message == "bye":
+                break
+
+    def receive_message(self):
+      while True:
+          try:
+              encrypted_message = self.socket.recv(1024)
+              message = self.cipher.decrypt(encrypted_message).decode()
+
+              # Check if the message is an acknowledgment
+              if message.startswith("ACK:"):
+                  # Remove the "ACK:" prefix and display the message in green
+                  print("\033[1;32;40m" + message[4:] + "\033[0m")
+              else:
+                  # Display regular messages in red
+                  print("\033[1;31;40m" + message + "\033[0m")
+          except:
+              print("Disconnected from server.")
+              break
+
+
 if __name__ == '__main__':
-  Client('127.0.0.1', 7632)
+    Client('127.0.0.1', 7632)
--- server.py
+++ server.py
@@ -1,78 +1,67 @@
 import socket
 from threading import Thread
+from cryptography.fernet import Fernet
 
 class Server:
     Clients = []
+    key = Fernet.generate_key()  # Or use a pre-shared key
+    cipher = Fernet(key)
 
-    # Create a TCP socket over IPv4. Accept at max 5 connections.
     def __init__(self, HOST, PORT):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((HOST, PORT))
         self.socket.listen(5)
         print('Server waiting for connection....')
+        print(f"Encryption Key (share with clients): {self.key.decode()}")
 
-    # Listen for connections on the main thread. When a connection
-    # is received, create a new thread to handle it and add the client
-    # to the list of clients.
     def listen(self):
         while True:
             client_socket, address = self.socket.accept()
             print("Connection from: " + str(address))
-            
-            # The first message will be the username
-            client_name = client_socket.recv(1024).decode()
+
+            client_name = self.receive_message(client_socket)
             client = {'client_name': client_name, 'client_socket': client_socket}
-            
-            # Broadcast that the new client has connected
-            self.broadcast_message(client_name, client_name + " has joined the chat!")
-            
+
+            self.broadcast_message(client_name, f"{client_name} has joined the chat!")
             Server.Clients.append(client)
-            Thread(target = self.handle_new_client, args = (client,)).start()
+            Thread(target=self.handle_new_client, args=(client,)).start()
 
     def handle_new_client(self, client):
         client_name = client['client_name']
         client_socket = client['client_socket']
         while True:
             try:
-                # Listen for incoming messages
-                client_message = client_socket.recv(1024).decode()
-                if not client_message.strip():
-                    raise ConnectionResetError
-
-                # If the message is "bye", disconnect the client
-                if client_message.strip() == f"{client_name}: bye":
+                client_message = self.receive_message(client_socket)
+                if client_message == "bye":
                     self.broadcast_message(client_name, f"{client_name} has left the chat!")
                     Server.Clients.remove(client)
                     client_socket.close()
                     break
 
-                # Broadcast the message to all other clients
-                self.broadcast_message(client_name, client_message, receipt=False)
-                
-                # Send a receipt confirmation back to the sender
-                self.broadcast_message(client_name, "Your message was received.", receipt=True)
+                self.broadcast_message(client_name, client_message)
+                self.send_message(client_socket, "Your message was received.", acknowledgment=True)
+
             except (ConnectionResetError, BrokenPipeError):
                 self.broadcast_message(client_name, f"{client_name} has left the chat unexpectedly.")
                 Server.Clients.remove(client)
                 client_socket.close()
                 break
 
-    # Loop through the clients and send the message down each socket.
-    # Skip the socket if it's the same client.
-    def broadcast_message(self, sender_name, message, receipt=False):
+    def broadcast_message(self, sender_name, message):
         for client in self.Clients:
-            client_socket = client['client_socket']
-            client_name = client['client_name']
+            if client['client_name'] != sender_name:
+                self.send_message(client['client_socket'], f"{sender_name}: {message}")
 
-            if receipt:
-                # Send receipt only to the sender
-                if client_name == sender_name:
-                    client_socket.send(f"Server: {message}".encode())
-            else:
-                # Send the message to all other clients
-                if client_name != sender_name:
-                    client_socket.send(message.encode())
+    def send_message(self, client_socket, message, acknowledgment=False):
+        if acknowledgment:
+            message = "ACK:" + message  # Add "ACK:" prefix for acknowledgment messages
+        encrypted_message = self.cipher.encrypt(message.encode())
+        client_socket.send(encrypted_message)
+
+    def receive_message(self, client_socket):
+        encrypted_message = client_socket.recv(1024)
+        return self.cipher.decrypt(encrypted_message).decode()
 
 if __name__ == '__main__':
-  server = Server('127.0.0.1', 7632)
-  server.listen()
+    server = Server('127.0.0.1', 7632)
+    server.listen()
